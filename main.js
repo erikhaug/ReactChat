49fb32e136e23ed24bb4f4f82857e1b766872d0f
Marius Gundersen <Marius.Gundersen@ciber.com>
2014-09-15 23:26:24 +0200

    removed unused packages

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var http = require('http');
-var commandDispatcher = require('command-dispatcher');
-var EventEmitter = require('events').EventEmitter;
-
 var app = express();
 var server = http.Server(app);
-
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/www'));
-
 server.listen(1337);
 
 var messages = [];
