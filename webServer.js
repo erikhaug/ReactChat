044cc07d3778812bc1636f985fbcf8c96dfea4e9
Erik Haug <erik.haug@ciber.com>
2014-09-15 16:57:34 +0200

    Nyeste endringer

--- webServer.js
+++ webServer.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var http = require('http');
-var commandHandler = require('./server/commandHandler');
 var commandDispatcher = require('./server/commandDispatcher');
 var EventEmitter = require('events').EventEmitter;
-var socketEventHandler = require('./server/socketEventHandler');
 
 var app = express();
 var server = http.Server(app);
 
 var io = require('socket.io')(server);
-
-var dispatcher = commandDispatcher();
-var command = dispatcher.command;
 
 var messages = [];
 
@@ -22,15 +17,9 @@ server.listen(1337);
 io.on('connection', function (socket) {
 	socket.on('publishMessage', function(message){
 		messages.push(message);
-    	io.emit('newMessages', messages);
-  	});
-  	
-  	socket.emit('newClient', { title: 'Chat Room' });
-  	socket.emit('newMessages', messages);
-});
-
-function pipeSocketToCommand(socket, name, command){
-  socket.on(name, function(data){
-    command(name, data);
+    io.emit('newMessages', messages);
   });
-}
+  	
+  socket.emit('newClient', { title: 'Chat Room' });
+  socket.emit('newMessages', messages);
+});
