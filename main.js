d59c10efe0539ee5808c63cd1b987adb5ab0c6be
Marius Gundersen <Marius.Gundersen@ciber.com>
2014-09-15 18:17:09 +0200

    Made name mandatory on login on the server too

--- main.js
+++ main.js
@@ -16,15 +16,17 @@ app.use(express.static(__dirname + '/www'));
 server.listen(1337);
 
 io.on('connection', function (socket) {
-	socket.on('publishMessage', function(message){
-		messages.push(message);
+  socket.on('publishMessage', function(message){
+    messages.push(message);
     io.emit('newMessages', messages);
   });
 
   socket.on('newUser', function(user){
-    users.push(user.name);
-    io.emit('newUserAdded', user);
-    socket.emit('userAllowedAccess', user);
-    socket.emit('newMessages', messages);
+    if(user.name.length){
+      users.push(user.name);
+      io.emit('newUserAdded', user);
+      socket.emit('userAllowedAccess', user);
+      socket.emit('newMessages', messages);
+    }
   });
 });
